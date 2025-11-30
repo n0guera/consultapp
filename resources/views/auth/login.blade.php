e1c6281213d2877c50dccb300200ab6ee000ccb6
Santiago Noguera <santiiago.noguera@gmail.com>
2025-11-29 21:47:36 -0300

    feat(ui): reworkear vista Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,79 @@
 <x-guest-layout>
+    <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <div class="flex justify-center items-center self-stretch gap-2">
+        <x-lucide-apple class="w-5 h-5" />
+        <p class="h-5 text-base text-center text-slate-900">
+            ConsultApp
+        </p>
+    </div>
+    <div class="flex flex-col justify-start items-start self-stretch gap-1.5">
+        <div class="flex flex-col justify-start items-center self-stretch">
+            <p class="text-lg text-center text-slate-900">
+                Iniciar Sesión en ConsultApp
+            </p>
+        </div>
+        <div class="flex flex-col justify-start items-center self-stretch">
+            <p class="text-sm text-center text-gray-400">
+                Accede para gestionar tu Agenda y Pacientes
+            </p>
+        </div>
+    </div>
+
+    <form method="POST" action="{{ route('login') }}" class="flex flex-col w-full gap-2.5">
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="flex flex-col gap-1.5">
+            <x-input-label for="email" value="Correo Electrónico" class="text-slate-900" />
+            <div class="flex items-center">
+                <x-lucide-mail class="ml-3 w-4 h-4 text-gray-400 absolute" />
+                <x-text-input id="email" class="pl-8 rounded-xl block w-full" type="email" name="email"
+                    :value="old('email')" required autofocus autocomplete="username" placeholder="nombre@correo.com" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="flex flex-col gap-1.5">
+            <x-input-label for="password" value="Contraseña" class="text-slate-900" />
+            <div class="flex items-center">
+                <x-lucide-lock class="ml-3 w-4 h-4 text-gray-400 absolute" />
+                <x-text-input id="password" class="pl-8 rounded-xl block w-full" type="password" name="password"
+                    required autocomplete="current-password" placeholder="••••••••" />
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="flex justify-between">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox"
+                    class="w-4 h-4 rounded-md bg-white border border-zinc-200 shadow-sm focus:ring-indigo-500"
+                    name="remember">
+                <span class="ms-2 text-sm text-slate-900">Recuérdame</span>
             </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                <a class="flex justify-start items-center gap-1.5 px-3 py-2 text-sm rounded-xl bg-white border border-zinc-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('password.request') }}">
+                    <x-lucide-circle-question-mark class="w-4 h-4" />
+                    ¿Olvidaste tu contraseña?
                 </a>
             @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
         </div>
+
+        <button type="submit"
+            class="flex justify-center items-center self-stretch gap-2 px-3 py-3 rounded-xl bg-teal-600">
+            <x-lucide-log-in class="w-4 h-4 text-white" />
+            <p class="text-sm text-center text-white">
+                Iniciar Sesión
+            </p>
+        </button>
     </form>
+    <p class="self-stretch text-xs text-center text-gray-400">
+        v1.0
+    </p>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,13 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="flex flex-col justify-start items-start w-[420px] gap-4 p-5 rounded-3xl bg-white border border-teal-700" style="box-shadow: 0px 4px 12px 0 rgba(0,0,0,0.25);">
                 {{ $slot }}
             </div>
         </div>
